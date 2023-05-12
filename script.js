f5173354c3c607dd544940c17de564ca3a8e6cdc
BlueDoraemon <3803480+BlueDoraemon@users.noreply.github.com>
2023-05-12 11:39:35 +1000

    Add operate function

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ function divide(a,b) {
 function rounding(number) {
     return number.toFixed(8); //display how many decimal places
 }
+
+let display = "";
+
+let result = 0;
+
+let operator = "";
+
+let operand = 0;
+
+function operate(result,operator,operand) {
+    switch(operator) {
+        case "+": return add(result,operand); 
+             break;
+        case "-": return subtract(result,operand);
+             break;
+        case "*": return multiply(result,operand); 
+            break;
+        case "/": return divide(result,operand); 
+            break;
+        case "=": return result;
+        default : display = `ERROR`;
+    }
+}
+
+//test
+console.log(operate(0,'+',5));
+
